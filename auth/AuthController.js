1396f2a0f21a6e22020d32139f01d0ca21cf1a3c
kadi <eng.ahmadkadi@gmail.com>
2018-10-03 11:21:33 +0200

    change me get to return the full actual user

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -35,7 +35,12 @@ router.post('/register', function(req, res) {
     jwt.verify(token, config.secret, function(err, decoded) {
       if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
       
-      res.status(200).send(decoded);
+      User.findById(decoded.id, function (err, user) {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+        
+        res.status(200).send(user);
+      });
     });
   });
 
