018c049fe4636028138a608fb1538a1d1860dfb1
kadi <eng.ahmadkadi@gmail.com>
2018-10-03 13:13:35 +0200

    add ref to use middleware (VerifyToken.js)

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -9,6 +9,8 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config');
 
+var VerifyToken = require('./VerifyToken');
+
 router.post('/register', function (req, res) {
 
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
@@ -28,20 +30,12 @@ router.post('/register', function (req, res) {
     });
 });
 
-router.get('/me', function (req, res) {
-  var token = req.headers['x-access-token'];
-  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+router.get('/me', VerifyToken, function (req, res, next) {
+  User.findById(req.userId, { password: 0 }, function (err, user) {
+    if (err) return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
 
-  jwt.verify(token, config.secret, function (err, decoded) {
-    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-
-    User.findById(decoded.id,
-      { password: 0 }, // projection
-      function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
-        res.status(200).send(user);
-      });
+    res.status(200).send(user);
   });
 });
 
@@ -58,7 +52,7 @@ router.post('/login', function (req, res) {
   });
 });
 
-router.get('/logout', function(req, res) {
+router.get('/logout', function (req, res) {
   res.status(200).send({ auth: false, token: null });
 });
 module.exports = router;
